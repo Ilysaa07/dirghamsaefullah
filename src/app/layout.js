121424b70fa355062bdfc2ece3881be0082e49c7
Ilysaa07 <iliyasa121213@gmail.com>
2026-01-31 15:09:27 +0700

    fix seo

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,8 +12,16 @@ const outfit = Outfit({
 });
 
 export const metadata = {
+  metadataBase: new URL('https://dirghamsaefullah.com'),
   title: "PT. DIRGHAM SAEFULLAH PERKASA - Kontraktor & Supplier Umum",
   description: "Jasa Kontraktor Umum Profesional. Keunggulan konstruksi, renovasi, dan desain oleh PT. DIRGHAM SAEFULLAH PERKASA.",
+  keywords: ["Kontraktor Umum", "General Contractor", "Konstruksi Rumah", "Renovasi Bangunan", "Desain Interior", "Supplier Material", "Tangerang", "Jakarta", "Indonesia", "PT Dirgham Saefullah Perkasa", "Jasa Bangun Rumah"],
+  authors: [{ name: "PT. DIRGHAM SAEFULLAH PERKASA" }],
+  creator: "PT. DIRGHAM SAEFULLAH PERKASA",
+  publisher: "PT. DIRGHAM SAEFULLAH PERKASA",
+  alternates: {
+    canonical: '/',
+  },
   openGraph: {
     title: "PT. DIRGHAM SAEFULLAH PERKASA - Kontraktor & Supplier Umum",
     description: "Mitra terpercaya Anda untuk keunggulan konstruksi dan renovasi.",
@@ -35,11 +43,44 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+  const jsonLd = {
+    "@context": "https://schema.org",
+    "@type": "GeneralContractor",
+    "name": "PT. DIRGHAM SAEFULLAH PERKASA",
+    "image": "https://dirghamsaefullah.com/logo.webp",
+    "description": "Jasa Kontraktor Umum Profesional. Keunggulan konstruksi, renovasi, dan desain oleh PT. DIRGHAM SAEFULLAH PERKASA.",
+    "url": "https://dirghamsaefullah.com",
+    "telephone": "+6289691118807",
+    "address": {
+      "@type": "PostalAddress",
+      "streetAddress": "Jalan Benteng Jaya",
+      "addressLocality": "Tangerang",
+      "addressCountry": "ID"
+    },
+    "openingHoursSpecification": {
+      "@type": "OpeningHoursSpecification",
+      "dayOfWeek": [
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday"
+      ],
+      "opens": "08:00",
+      "closes": "17:00"
+    },
+    "priceRange": "$$"
+  };
+
   return (
     <html lang="id">
       <body
         className={`${inter.variable} ${outfit.variable} antialiased`}
       >
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+        />
         {children}
       </body>
     </html>
